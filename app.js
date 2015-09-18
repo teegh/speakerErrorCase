a9dae9db766ee06e7b4b782a251c6b8582dae2a8
gusen <gamagori-usen@gamagori-usen-no-iMac.local>
2015-09-18 09:14:29 +0900

    addsp

--- app.js
+++ app.js
@@ -12,6 +12,21 @@ var AV = require("av");
 require("mp3");
 var obj;
 
+
+var Speaker = require('speaker');
+
+// Create the Speaker instance
+var speaker = new Speaker({
+  channels: 2,          // 2 channels
+  bitDepth: 16,         // 16-bit samples
+  sampleRate: 44100     // 44,100 Hz sample rate
+});
+
+// PCM data from stdin gets piped into the speaker
+process.stdin.pipe(speaker);
+
+
+
 //サーバーにアクセスする。
 function connectAndPlay(){
     request(url, function (error, response, body) {
